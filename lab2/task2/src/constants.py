683b0e50be26b8f125f0f0489a545fc4eecfd2af
timofey <tima021003tima@gmail.com>
2023-03-27 16:43:48 +0300

    Регулярки, доступные команды, строка для help

---
+++ lab2/task2/src/constants.py
@@ -0,0 +1,11 @@
+import re
+
+AVAILABLE_COMMANDS = ['add', 'remove', 'find',
+                      'list', 'grep', 'save', 'load', 'switch', 'help', 'exit']
+
+HELP_INFO = 'Available commands: help - switch - add - remove - find - list - grep - save - load - exit\nEnter command name and pass parameters (in double quotes if you want to group them).\nswitch <username> -> switches to user or creates if does not exist\nadd [params] -> add params to current user storage\nremove <param> -> remove param from current user storage\nfind [params] -> prints all found params or message that there are no such params\nlist -> prints current user storage\ngrep <pattern> -> search for items in storage that match a pattern\nexit -> quit program'
+
+COMMAND_PATTERN = re.compile(
+    r'(?<!\S)\b(?:add|remove|find|grep|save|load|list|switch|help|exit)\b(?=(?:(?:[^"]*"){2})*[^"]*$)(?<!")')
+
+ARGS_PATTERN = re.compile(r'("[^"]+"|\S+)')
