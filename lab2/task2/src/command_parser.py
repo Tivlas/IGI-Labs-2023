c10b6faf406736f945c15b74a4a6f145814ffdbe
Tivlas <tima021003tima@gmail.com>
2023-03-28 13:02:28 +0300

    Кавычки не захватываются в аргумент

--- lab2/task2/src/command_parser.py
+++ lab2/task2/src/command_parser.py
@@ -2,6 +2,7 @@ from constants import COMMAND_PATTERN, ARGS_PATTERN
 from comand_args_validator import Validator
 from itertools import zip_longest
 
+
 class CommandParser:
 
     @staticmethod
@@ -10,12 +11,15 @@ class CommandParser:
             raise ValueError("Wrong number of quotes!")
 
         commands = COMMAND_PATTERN.findall(input)
-        args = list(filter(lambda arg: arg != '',
-                    COMMAND_PATTERN.split(input)))
+        all_args = list(filter(lambda arg: arg != '',
+                               COMMAND_PATTERN.split(input)))
 
         separated_args = []
-        for arg in args:
-            separated_args.append(ARGS_PATTERN.findall(arg))
+        for specific_comm_args_as_one_string in all_args:
+            specific_comm_args = ARGS_PATTERN.findall(
+                specific_comm_args_as_one_string)
+            separated_args.append(
+                [arg if arg[0] != '"' else arg[1:-1] for arg in specific_comm_args])
 
         commands_and_args = list(zip_longest(commands, separated_args))
 
