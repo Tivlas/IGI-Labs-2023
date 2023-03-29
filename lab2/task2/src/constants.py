e5555840ccff217d0064808e33733e9339a9582a
timofey <tima021003tima@gmail.com>
2023-03-29 21:08:31 +0300

    Updated HELP_INFO structure

--- lab2/task2/src/constants.py
+++ lab2/task2/src/constants.py
@@ -1,6 +1,30 @@
 import re
+from colorama import Fore, Style
 
-HELP_INFO = 'Available commands: help - switch - add - remove - find - list - grep - save - load - exit\nEnter command name and pass parameters (between "text1 text2" if you want to group them).\nswitch <username> -> switches to user or creates if does not exist\nadd [params] -> add params to current user storage\nremove <param> -> remove param from current user storage\nfind [params] -> prints all found params or message that there are no such params\nlist -> prints current user storage\ngrep <pattern> -> search for items in storage that match a pattern\nexit -> quit program'
+green = Fore.GREEN
+default = Style.RESET_ALL
+
+SWITCH_INFO = f"{green}switch{default} <username> — switches to user or creates if such user does not exist. Takes 1 parameter."
+
+ADD_INFO = f"{green}add{default} [parameters] — add parameters to current user storage. Takes at least 1 parameter."
+
+REMOVE_INFO = f"{green}remove{default} <parameter> — removes 1 item from current user storage. Takes 1 parameter."
+
+FIND_INFO = f"{green}find{default} [parameters] — prints all found parameters or message if no parameters found. Takes at least 1 parameter."
+
+LIST_INFO = f"{green}list{default} — prints current user storage items. Takes 0 parameters."
+
+GREP_INFO = f"{green}grep{default} <pattern> — search for items in current user storage that match given pattern. Takes 1 parameter."
+
+LOAD_INFO = f"{green}load{default} None | <path> — loads data from default file (selected by the program) or user-provided file. Takes 0 or 1 parameter."
+
+SAVE_INFO = f"{green}save{default} None | <path> — save data to default file (selected by the program) or user-provided file. Takes 0 or 1 parameter."
+
+EXIT_INFO = f"{green}exit{default} — quits the program. Takes 0 parameters."
+
+ADDITIONAL_INFO = f"{green}Note:{default} command name cannot be a parameter to another command!"
+
+HELP_INFO = f"\n{SWITCH_INFO}\n\n{ADD_INFO}\n\n{REMOVE_INFO}\n\n{FIND_INFO}\n\n{LIST_INFO}\n\n{GREP_INFO}\n\n{LOAD_INFO}\n\n{SAVE_INFO}\n\n{EXIT_INFO}\n\n{ADDITIONAL_INFO}"
 
 COMMAND_PATTERN = re.compile(
     r'(?<!\S)\b(?:add|remove|find|grep|save|load|list|switch|help|exit)\b(?=(?:(?:[^"]*"){2})*[^"]*$)(?<!")')
