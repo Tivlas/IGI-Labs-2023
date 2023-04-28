641dfab05b714f6948e324623798843ff55e7017
Tivlas <tima021003tima@gmail.com>
2023-04-29 00:39:31 +0300

    Added dict serialization function. Added docstrings to functions.

--- lab3/serializers/src/serialization.py
+++ lab3/serializers/src/serialization.py
@@ -4,16 +4,74 @@ from . import constants
 def get_specific_serializer_function(obj):
     if isinstance(obj, int | float | str | bool | complex | type(None)):
         return serialize_primitive_type
+    if isinstance(obj, dict):
+        return serialize_dict
 
 
-def serialize(obj):
+def serialize(obj) -> tuple:
+    """ 
+    Function:
+    -----------
+    Serializes any type
+
+    Parameters:
+    -----------
+           - obj: object to serialize
+
+    Returns:
+    -----------
+          - tuple of tuples: {type, value}
+    """
     serializer = get_specific_serializer_function(obj)
     serialized_obj = serializer(obj)
-    return tuple((k, serialized_obj[k]) for k in serialized_obj)
+    result = tuple((k, serialized_obj[k]) for k in serialized_obj)
+    return result
 
 
 def serialize_primitive_type(obj: float | bool | int | complex | str | None) -> dict:
+    """ 
+    Function:
+    -----------
+    Serializes built in primitive types
+
+    Parameters:
+    -----------
+           - obj (float | bool | int | complex | str | None): object to serialize
+
+    Returns:
+    -----------
+          - dict: {type: value}
+    """
     serialized_primitive_type = dict()
     serialized_primitive_type[constants.TYPE] = type(obj).__name__
     serialized_primitive_type[constants.VALUE] = obj
     return serialized_primitive_type
+
+
+def serialize_dict(obj: dict) -> dict:
+    """ 
+    Function:
+    -----------
+    Serializes dict
+
+    Parameters:
+    -----------
+           - obj (dict): object to serialize
+
+    Returns:
+    -----------
+          - dict of tuples: [((type: name), (value: v))] = ((type: name), (value: v))
+    """
+    serialized_obj = dict()
+    serialized_obj[constants.TYPE] = constants.DICT
+    serialized_obj[constants.VALUE] = {}
+
+    for k, v in obj.items():
+        key = serialize(k)
+        value = serialize(v)
+        serialized_obj[constants.VALUE][key] = value
+
+    serialized_obj[constants.VALUE] = tuple((k, serialized_obj[constants.VALUE][k])
+                                            for k in serialized_obj[constants.VALUE])
+
+    return serialized_obj
