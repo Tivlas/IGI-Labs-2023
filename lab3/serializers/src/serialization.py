d2d7962918bdc9c76bf4c0ee4eb440b15e3ff643
Tivlas <tima021003tima@gmail.com>
2023-04-28 19:02:39 +0300

    Started to implement primitive types serialization.

---
+++ lab3/serializers/src/serialization.py
@@ -0,0 +1,19 @@
+import constants
+
+
+def get_specific_serializer_function(obj):
+    if isinstance(obj, int | float | str | bool | complex | type(None)):
+        return serialize_primitive_type
+
+
+def serialize(obj):
+    serializer = get_specific_serializer_function(obj)
+    serialized_obj = serializer(obj)
+    return tuple((k, serialized_obj[k]) for k in serialized_obj)
+
+
+def serialize_primitive_type(obj: float | bool | int | complex | str | None) -> dict:
+    serialized_primitive_type = dict()
+    serialized_primitive_type[constants.TYPE] = type(obj).__name__
+    serialized_primitive_type[constants.VALUE] = obj
+    return serialized_primitive_type
