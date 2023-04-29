9b1b3808853eaab98d51469b6aa0d3bac382354b
Tivlas <tima021003tima@gmail.com>
2023-04-29 12:40:08 +0300

    Added docstrings.

--- lab3/serializers/json_serializer/JsonSerializer.py
+++ lab3/serializers/json_serializer/JsonSerializer.py
@@ -5,8 +5,35 @@ from .serialization_logic import serialize_to_json
 class JsonSerializer:
     @staticmethod
     def dumps(obj) -> str:
+        """ 
+        Function:
+        -----------
+        Serializes to json string
+
+        Parameters:
+        -----------
+            - obj: object to serialize
+
+        Returns:
+        -----------
+            - string
+        """
         serialized_obj = serialize(obj)
         return serialize_to_json(serialized_obj)
 
-    def dump(obj, file):
+    @staticmethod
+    def dump(obj, file) -> None:
+        """ 
+        Function:
+        -----------
+        Serializes and writes to file
+
+        Parameters:
+        -----------
+            - obj: object to serialize
+
+        Returns:
+        -----------
+            - None
+        """
         file.write(JsonSerializer.dumps(obj))
