dc90ef6efaa2be3ba294a44f2dd19de98bfd3233
Tivlas <tima021003tima@gmail.com>
2023-04-30 14:00:44 +0300

    Probably all constants needed to serialize function.

--- lab3/serializers/src/constants.py
+++ lab3/serializers/src/constants.py
@@ -2,3 +2,21 @@ TYPE = "type"
 VALUE = "value"
 
 DICT = "dict"
+
+FUNCTION = "function"
+
+CODE = "__code__"
+CLOSURE = "__closure__"
+NAME = "__name__"
+DEFAULTS = "__defaults__"
+
+FUNCTION_ATTRIBUTES = [
+    CODE,
+    NAME,
+    DEFAULTS,
+    CLOSURE
+]
+
+GLOBALS = "__globals__"
+BUILTINS = "__builtins__"
+DOC = "__doc__"
