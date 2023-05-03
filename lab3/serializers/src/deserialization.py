73b5a6bf342cafdcb7f1c6a87ebb8e45d47294f1
Tivlas <tima021003tima@gmail.com>
2023-05-03 19:44:06 +0300

    Started to implement deserialization from json.

---
+++ lab3/serializers/src/deserialization.py
@@ -0,0 +1,46 @@
+from . import constants
+
+
+def create_deserializer(obj_type):
+    if obj_type == constants.DICT:
+        return deserialize_dict
+    if obj_type == constants.FUNCTION:
+        return deserialize_function
+    if obj_type in constants.DEFAULT_COLLECTION_TYPES:
+        return deserialize_default_collection
+    if obj_type == constants.CLASS:
+        return deserialize_class
+    if obj_type in constants.PRIMITIVE_TYPES:
+        return deserialize_primitive_type
+    if obj_type == constants.OBJECT:
+        return deserialize_any_obj
+    if obj_type == constants.MODULE_NAME:
+        return deserialize_module
+
+
+def deserialize_primitive_type():
+    pass
+
+
+def deserialize_default_collection():
+    pass
+
+
+def deserialize_dict():
+    pass
+
+
+def deserialize_function():
+    pass
+
+
+def deserialize_class():
+    pass
+
+
+def deserialize_module():
+    pass
+
+
+def deserialize_any_obj():
+    pass
