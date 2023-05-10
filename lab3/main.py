506f6ff8116a92728cec05232e0441d5e37abdbe
Tivlas <tima021003tima@gmail.com>
2023-05-10 21:27:08 +0300

    CLI.

--- lab3/main.py
+++ lab3/main.py
@@ -1,21 +1,41 @@
 from serializers.json_serializer.JsonSerializer import JsonSerializer
-from serializers.factory import SerializersFactory
-from os import path, getcwd
+from serializers.xml_serializer.XmlSerializer import XmlSerializer
 
+while (True):
+    print('1: deserialize from json')
+    print('2: deserialize from xml')
+    print('3: json to xml')
+    print('4: xml to json')
 
-def create_path(path_part: str):
-    return path.join(getcwd(), "temp_serialized_files", path_part)
+    choice = input()
+    match choice:
+        case '1':
+            file_name = input('Input file name: ')
+            with open(file_name, 'r') as f:
+                print(JsonSerializer.load(f))
 
+        case '2':
+            file_name = input("input file name: ")
+            with open(file_name, 'r') as f:
+                print(XmlSerializer.load(f))
 
-sz = SerializersFactory.create_serializer("JSON")
-a = None
-with open(create_path("primitive_type.json"), 'w', encoding='utf-8') as f:
-    sz.dump(a, f)
+        case '3':
+            file_name = input("input json file name: ")
+            xml = input('input xml file name: ')
+            with open(file_name, 'r') as f:
+                obj = JsonSerializer.load(f)
+            with open(xml, 'w', encoding='utf-8') as f:
+                XmlSerializer.dump(obj, f)
 
-dct = {"a": 4, "b": 3, 3: "string"}
-with open(create_path("dict.json"), 'w', encoding='utf-8') as f:
-    sz.dump(dct, f)
+        case '4':
+            file_name = input("input xml file name: ")
+            json = input('input json file name: ')
+            with open(file_name, 'r') as f:
+                obj = XmlSerializer.load(f)
+            with open(json, 'w', encoding='utf-8') as f:
+                JsonSerializer.dump(obj, f)
 
-default_collection = [1, 2, 3, "string value"]
-with open(create_path("default_collection.json"), 'w', encoding='utf-8') as f:
-    sz.dump(default_collection, f)
+        case "exit()":
+            exit()
+        case _:
+            print('incorrect')
