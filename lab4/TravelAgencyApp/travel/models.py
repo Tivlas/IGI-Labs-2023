efccf8ceb12ab5c2a97b73d7a05b6dedb1cf9db6
Tivlas <tima021003tima@gmail.com>
2023-05-21 22:35:33 +0300

    Начал определять другие модели.

--- lab4/TravelAgencyApp/travel/models.py
+++ lab4/TravelAgencyApp/travel/models.py
@@ -1,25 +1,52 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+from datetime import date, timedelta
 
 
 class Client(models.Model):
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255)
-    email = models.EmailField(unique=True)
-    phone_number = models.CharField(max_length=20)
-    date_of_birth = models.DateField()
-    address = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=255, help_text="Enter first name")
+    last_name = models.CharField(max_length=255, help_text="Enter last name")
+    email = models.EmailField(unique=True, help_text='something@gmail.com')
+    phone_number = models.CharField(
+        max_length=20, help_text='+375 (29) 123-45-67')
+    date_of_birth = models.DateField(validators=[MinValueValidator(
+        limit_value=date.today() - timedelta(days=18*365))])
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
 
 
-class Hotel(models.Model):
-    pass
+class Season(models.Model):
+    name = models.CharField(max_length=6)
+    climate_description = models.CharField(max_length=100)
 
-
-class Trip(models.Model):
-    pass
+    def __str__(self) -> str:
+        return self.name
 
 
 class Country(models.Model):
-    pass
+    name = models.CharField(max_length=50)
+    seasons = models.ManyToManyField(Season, related_name='countries')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Hotel(models.Model):
+    name = models.CharField(max_length=50)
+    stars = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)])
+    price_per_day = models.DecimalField(default=0.0)
+    country = models.ForeignKey(Country, related_name='hotels')
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+
+class Trip(models.Model):
+    country = models.ForeignKey(Country)
+    duration = models.IntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(4)])
+    chosen_hotel = models.ForeignKey(Hotel, related_name='trips')
+    departure_date = models.DateField()
+    total_cost = models.DecimalField()
