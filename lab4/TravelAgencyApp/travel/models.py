2fbea621098a0c48903e149b56db7f3fd7af8486
Tivlas <tima021003tima@gmail.com>
2023-05-21 21:08:26 +0300

    Удалил модель адреса и добавил шаблоны для остальных моделей.

--- lab4/TravelAgencyApp/travel/models.py
+++ lab4/TravelAgencyApp/travel/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 
 
-class Address(models.Model):
-    street = models.CharField(max_length=255)
-    city = models.CharField(max_length=255)
-    country = models.CharField(max_length=255)
-
-    def __str__(self):
-        return f"{self.street}, {self.city}, {self.country}"
-
-
 class Client(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20)
     date_of_birth = models.DateField()
-    address = models.OneToOneField(Address, on_delete=models.CASCADE)
+    address = models.CharField(max_length=100)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class Hotel(models.Model):
+    pass
+
+
+class Trip(models.Model):
+    pass
+
+
+class Country(models.Model):
+    pass
