3c2ea9a0e9bcb8e939e97dc88be5664ea272bc75
Tivlas <tima021003tima@gmail.com>
2023-05-22 20:08:49 +0300

    Зарегал страны, отели, описание климата.

--- lab4/TravelAgencyApp/travel/admin.py
+++ lab4/TravelAgencyApp/travel/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import SeasonClimateDescription, Country, Trip, Hotel
 
 # Register your models here.
+
+
+@admin.register(SeasonClimateDescription)
+class SeasonClimateDescriptionAdmin(admin.ModelAdmin):
+    list_display = ['season_name', 'climate_description']
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    filter_horizontal = ['climate_descriptions']
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'stars', 'price_per_day', 'country']
+    list_filter = ['stars', 'country']
