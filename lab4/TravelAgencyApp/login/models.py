1c85eae7dd9282c95813991c61b3aeb542ae2c6b
Tivlas <tima021003tima@gmail.com>
2023-05-23 18:45:01 +0300

    Заготовка для регистрации.

---
+++ lab4/TravelAgencyApp/login/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class MyUser(AbstractUser):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=50)
+    REQUIRED_FIELDS = ['first_name', 'last_name',
+                       'email', 'date_of_birth', 'phone_number']
+    USERNAME_FIELD = ['username']
