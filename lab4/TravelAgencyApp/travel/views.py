73f29d1e8c5f7dc29a35882c63eab158e5d1f340
Tivlas <tima021003tima@gmail.com>
2023-05-23 23:32:37 +0300

    Добавил сортировки по цене, фильтрацию по звездам и цене.

--- lab4/TravelAgencyApp/travel/views.py
+++ lab4/TravelAgencyApp/travel/views.py
@@ -9,9 +9,26 @@ from .forms import TripForm
 def list_trips(request, trip_country_name=None):
     trips = Trip.objects.all()
     country = None
+    sort = request.GET.get('sort')
+    min_cost = request.GET.get('min_cost')
+    max_cost = request.GET.get('max_cost')
+    min_stars = request.GET.get('min_stars')
+    max_stars = request.GET.get('max_stars')
+
     if trip_country_name:
         country = get_object_or_404(Country, name=trip_country_name)
         trips = trips.filter(country=country)
+
+    if min_cost and max_cost:
+        trips = trips.filter(total_cost__gte=min_cost, total_cost__lte=max_cost)
+
+    if min_stars and max_stars:
+        trips = trips.filter(chosen_hotel__stars__gte=min_stars, chosen_hotel__stars__lte=max_stars)
+
+    if sort == 'ascending':
+        trips = trips.order_by('total_cost')
+    elif sort == 'descending':
+        trips = trips.order_by('-total_cost')
 
     countries = Country.objects.all()
     return render(request, 'trips/list_trips.html', {'trips': trips, 'countries': countries, 'country': country})
