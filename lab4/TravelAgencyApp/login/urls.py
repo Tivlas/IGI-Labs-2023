f3ea38d033b4a4fc22332d0394937df0bc4b2b6e
Tivlas <tima021003tima@gmail.com>
2023-05-25 22:04:46 +0300

    Добавил urls.

--- lab4/TravelAgencyApp/login/urls.py
+++ lab4/TravelAgencyApp/login/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'login'
 
 urlpatterns = [
-
+    path('signup/', views.SignUpFormView.as_view(), name='signup'),
+    path('signin/', views.SignInFormView.as_view(), name='signin'),
+    path('logout/', views.LogoutFormView.as_view(), name='logout')
 ]
