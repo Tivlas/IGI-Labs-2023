be5ec0ce095d4b2fd0a4ecd13ec8f1201beddd58
Tivlas <tima021003tima@gmail.com>
2023-05-27 19:11:52 +0300

    view для добавления в карту.

--- lab4/TravelAgencyApp/cart/views.py
+++ lab4/TravelAgencyApp/cart/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
+from travel.models import Trip
+from .cart import Cart
+from .forms import AddTripForm
 
-# Create your views here.
+
+@require_POST
+def cart_add(request, trip_id):
+    cart = Cart(request)
+    trip = get_object_or_404(Trip, id=trip_id)
+    form = AddTripForm(request.POST)
+    if form.is_valid():
+        form_data = form.cleaned_data
+        cart.add(trip=trip,
+                 quantity=form_data['quantity'],
+                 update_quantity=form_data['update'])
+    return redirect('cart:cart_details')
