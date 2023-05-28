5a73e9c8bf6ccc96d1b903ce54caddd628735e7f
Tivlas <tima021003tima@gmail.com>
2023-05-28 13:25:49 +0300

    view для создания заказа.

--- lab4/TravelAgencyApp/order/views.py
+++ lab4/TravelAgencyApp/order/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import OrderItem
+from cart.cart import Cart
+from .models import Order
+from django.core.exceptions import PermissionDenied
 
-# Create your views here.
+
+def create_order(request):
+    if not request.user.is_authenticated:
+        raise PermissionDenied("Permission denied. Sign in first.")
+
+    cart = Cart(request)
+    if request.method == 'POST':
+        order = Order.objects.create(client=request.user)
+
+        for item in cart:
+            OrderItem.objects.create(order=order,
+                                     trip=item['trip'],
+                                     cost=item['cost'],
+                                     quantity=item['quantity'])
+        cart.clear()
+        return render(request, 'order_created.html')
+
+    return render(request, 'create_order.html',
+                  {'cart': cart})
