040a507248b59fb1556e8424613ed1edba6c0224
Tivlas <tima021003tima@gmail.com>
2023-05-30 19:41:11 +0300

    Пользователи и заказы.

--- lab4/TravelAgencyApp/my_statistics/views.py
+++ lab4/TravelAgencyApp/my_statistics/views.py
@@ -1,4 +1,23 @@
 from django.shortcuts import render
+from django.db.models import Sum, F
+from django.db.models import Count
+from django.db.models.functions import ExtractYear
+from travel.models import Trip
+from order.models import Order
+from login.models import MyUser
+
 
 def statistics(request):
-    pass
+    clients = MyUser.objects.order_by('last_name', 'first_name')
+    client_data = []
+
+    for client in clients:
+        orders = client.order_set.all()
+        total_sales = orders.annotate(
+            total_cost=Sum(F('items__quantity') * F('items__cost'))
+        ).aggregate(total_sales=Sum('total_cost'))['total_sales']
+        client_data.append(
+            {'client': client, 'orders': orders, 'total_sales': total_sales})
+
+    context = {'client_data': client_data}
+    return render(request, 'statistics.html', context)
